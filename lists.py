b6c311e58ee5b1b4e15850e2348ac4e68da65588
Sonia-Murei <mureisonia@gmail.com>
2026-04-15 16:26:18 +0300

    lists

---
+++ lists.py
@@ -0,0 +1,12 @@
+# Create a list of fruits
+fruits = ['Mango', 'Banana', 'Orange','Lemon']
+print(type(fruits))
+print(fruits)
+print(fruits[1])
+
+# Update items in the list
+fruits[1]='Apple'
+print(fruits)
+
+# Slice
+print(fruits[2:4])
