1fed68f19f1cb36fbb97cf60dab748c27508741b
Sonia-Murei <mureisonia@gmail.com>
2026-04-15 15:18:30 +0300

    float task corrections

--- task2.py
+++ task2.py
@@ -1,4 +1,3 @@
-# NB: Use string  slice & concatenation, but have result as float 
 # Convert a float to an integer with an inbuilt function in Python temp = 56.8926 to 57
 temp = 56.8926
 temp=round(temp)
@@ -15,10 +14,19 @@ temp=round(temp,3)
 print(temp)
 
 # Convert the float below to give the results as follows temp=56.8926 to 8.926 
+# NB: Use string  slice & concatenation, but have result as float 
 temp = 56.8926
 temp=str(temp)
 temp=temp[3:]
 temp=float(temp)
 temp=temp/1000
+print(temp)
+
+# Correction
+temp = 56.8926
+temp=str(temp)
+temp=temp[3:]
+temp= temp[0]+"."+temp[1:]
+temp=float(temp)
 print(temp)
 
