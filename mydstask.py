dca9147fc69b2ed00be2d3d63730d42e8771a34e
Sonia-Murei <mureisonia@gmail.com>
2026-04-21 16:14:57 +0300

    Data Structures

--- mydstask.py
+++ mydstask.py
@@ -10,9 +10,20 @@ print(my_ds[3][1])
 my_ds[3][2]["amount"] = 90
 print(my_ds)
 # Reverse 987 to 789 without using an inbuilt -method or Assigning 789 manually: Hint: Strings can be reversed using [::]
-my_ds[4]=int(str(my_ds[4])[::-1])
+# convert to string
+my_ds[4]=str(my_ds[4])
+print(type(my_ds[4]))
+# reverse
+my_ds[4]=my_ds[4][::-1]
+# convert back to integer
+my_ds[4]=int(my_ds[4])
 print(my_ds)
 
 # Change the name "John" to "Jane".
-my_ds[5]=(76,"Jane")
+# convert tuple to list
+my_ds[5]=list(my_ds[5])
+# modify
+my_ds[5][1]="Jane"
+# convert back to tuple
+my_ds[5]=tuple(my_ds[5])
 print(my_ds)
